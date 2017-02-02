4163217b64a7565b1b0a87b2765eae7d4848aefa
wsims <willy.simz@gmail.com>
2017-02-01 17:28:07 -0800

    Added computer fire AI, updated MainTest

--- src/test/edu/oregonstate/cs361/battleship/MainTest.java
+++ src/test/edu/oregonstate/cs361/battleship/MainTest.java
@@ -16,12 +16,11 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 import static spark.Spark.awaitInitialization;
 
-
 /**
  * Created by michaelhilton on 1/26/17.
  */
 class MainTest {
-
+    String getModelTest = "{\"aircraftCarrier\":{\"name\":\"AircraftCarrier\",\"length\":5,\"start\":{\"Across\":0,\"Down\":0},\"end\":{\"Across\":0,\"Down\":0}},\"battleship\":{\"name\":\"Battleship\",\"length\":4,\"start\":{\"Across\":0,\"Down\":0},\"end\":{\"Across\":0,\"Down\":0}},\"cruiser\":{\"name\":\"Cruiser\",\"length\":3,\"start\":{\"Across\":0,\"Down\":0},\"end\":{\"Across\":0,\"Down\":0}},\"destroyer\":{\"name\":\"Destroyer\",\"length\":2,\"start\":{\"Across\":0,\"Down\":0},\"end\":{\"Across\":0,\"Down\":0}},\"submarine\":{\"name\":\"Submarine\",\"length\":2,\"start\":{\"Across\":0,\"Down\":0},\"end\":{\"Across\":0,\"Down\":0}},\"computer_aircraftCarrier\":{\"name\":\"Computer_AircraftCarrier\",\"length\":5,\"start\":{\"Across\":2,\"Down\":2},\"end\":{\"Across\":2,\"Down\":6}},\"computer_battleship\":{\"name\":\"Computer_Battleship\",\"length\":4,\"start\":{\"Across\":2,\"Down\":8},\"end\":{\"Across\":5,\"Down\":8}},\"computer_cruiser\":{\"name\":\"Computer_Cruiser\",\"length\":3,\"start\":{\"Across\":4,\"Down\":1},\"end\":{\"Across\":4,\"Down\":3}},\"computer_destroyer\":{\"name\":\"Computer_Destroyer\",\"length\":2,\"start\":{\"Across\":7,\"Down\":3},\"end\":{\"Across\":7,\"Down\":4}},\"computer_submarine\":{\"name\":\"Computer_Submarine\",\"length\":2,\"start\":{\"Across\":9,\"Down\":6},\"end\":{\"Across\":9,\"Down\":7}},\"playerHits\":[],\"playerMisses\":[],\"computerHits\":[],\"computerMisses\":[]}";
     @BeforeAll
     public static void beforeClass() {
         Main.main(null);
@@ -37,7 +36,7 @@ class MainTest {
     public void testGetModel() {
         TestResponse res = request("GET", "/model");
         assertEquals(200, res.status);
-        assertEquals("MODEL",res.body);
+        assertEquals(getModelTest,res.body);
     }
 
     @Test
@@ -45,6 +44,12 @@ class MainTest {
         TestResponse res = request("POST", "/placeShip/aircraftCarrier/1/1/horizontal");
         assertEquals(200, res.status);
         assertEquals("SHIP",res.body);
+    }
+
+    @Test
+    public void testCheckCollision() {
+        assertEquals(checkCollision(fire, model.computer_aircraftCarrier, model.computer_battleship, model.computer_cruiser, model.computer_destroyer, model.computer_submarine), True);
+
     }
 
     private TestResponse request(String method, String path) {
